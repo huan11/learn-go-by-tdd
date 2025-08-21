cbddec0514cc9feec79d8a48c6292d88c96ccdf8
Asher <17362772663@163.com>
2025-08-21 21:08:56 +0800

    [test code: refactor]: remove duplicate code in test code

--- hello_test.go
+++ hello_test.go
@@ -3,26 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello, Asher"
-	got := Hello("Asher")
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-
 	t.Run("saying hello to people", func(t *testing.T) {
 		want := "Hello, Asher"
 		got := Hello("Asher")
-		if want != got {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, want, got)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied ", func(t *testing.T) {
 		want := "Hello, world"
 		got := Hello("")
-		if want != got {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, want, got)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, want string, got string) {
+	if want != got {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
