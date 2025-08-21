9f60fda9cd359c67893b133421dbd48d4a21b26d
Asher <17362772663@163.com>
2025-08-21 20:46:57 +0800

    [write test first]:red
    ➜  learn-go-by-tdd git:(main) ✗ go test
    # github.com/huan11/learn-go-by-tdd [github.com/huan11/learn-go-by-tdd.test]
    ./hello_test.go:7:15: too many arguments in call to Hello
            have (string)
            want ()
    FAIL    github.com/huan11/learn-go-by-tdd [build failed]

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello"
-	got := Hello()
+	want := "Hello, Asher"
+	got := Hello("Asher")
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
