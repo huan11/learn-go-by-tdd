cba08d6f060017da4f5b95ff413ddfe2539a4466
Asher <17362772663@163.com>
2025-08-21 20:58:58 +0800

    [new requirement: red]:handle empty name
    when our function is called with an empty string it defaults to printing "Hello, World", rather than "Hello, ".
    
    --- FAIL: TestHello (0.00s)
        --- FAIL: TestHello/say_'Hello,_world'_when_an_empty_string_is_supplied_ (0.00s)
            hello_test.go:25: got "Hello, " want "Hello, world"
    FAIL
    exit status 1
    FAIL    github.com/huan11/learn-go-by-tdd       0.324s

--- hello_test.go
+++ hello_test.go
@@ -9,4 +9,20 @@ func TestHello(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		want := "Hello, Asher"
+		got := Hello("Asher")
+		if want != got {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, world' when an empty string is supplied ", func(t *testing.T) {
+		want := "Hello, world"
+		got := Hello("")
+		if want != got {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
