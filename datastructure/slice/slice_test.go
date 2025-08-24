f3f55de5cf9b63df20c02bf4e003338dbea9f82c
Asher <17362772663@163.com>
2025-08-24 08:53:23 +0800

    [data structure: slice] [refactor test code: clean duplicated code]

--- datastructure/slice/slice_test.go
+++ datastructure/slice/slice_test.go
@@ -3,17 +3,6 @@ package slice
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("sum the collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 2}
-
-		want := 5
-		got := Sum(numbers)
-
-		if got != want {
-			t.Errorf("want %d, got %d, %v", want, got, numbers)
-		}
-	})
-
 	t.Run("sum the collection of any numbers", func(t *testing.T) {
 		numbers := []int{2, 2, 2, 2, 2}
 
